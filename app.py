e9f91c77cc1c69aea2ee1fe006b9ad65a984de32
FlowerChat <flowerchatbot@gmail.com>
2017-05-27 23:07:34 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def makeWebhookResult(req):
                         
                         
                         {
-                            "title":"Hi, "+ facebook_user_firstname +", I can help you find the best local café",                            
+                            "title":"Hi, "+ facebook_user_firstname +", I can help you find the best local cafe",                            
                             "image_url":"http://www.fiorita.cz/wp-content/uploads/2017/03/kvetinarstvi-praha-jarni-kytice-tulipany-anemony-pryskyrniky.jpg",
                             "subtitle":"This bouquet was tied by my sponsors. You can order flowers through their web",
                             #}
@@ -122,7 +122,7 @@ def makeWebhookResult(req):
         
 
         facebook_message = {
-            "text":"Great!. For better service please share your current location and I will send you info about cafés nearby",
+            "text":"Great!. For better service please share your current location and I will send you info about cafes nearby",
             "quick_replies":[
                 {
                     "content_type":"location",
